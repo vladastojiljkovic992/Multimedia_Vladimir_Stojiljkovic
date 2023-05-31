5d731701443c82fec35a78d3d291645ab5d47237
unknown <vladimir.stojiljkovic992@gmail.com>
2023-05-31 19:17:18 +0200

    first commit

---
+++ src/main/java/com/main/main/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.main.main;
+
+/**
+ *
+ * @author stepa
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
